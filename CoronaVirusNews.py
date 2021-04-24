024a496895da3cd30b5da12951038ce2a940e90a
P3RPL3Xs <80137110+P3RPL3Xs@users.noreply.github.com>
2021-04-24 17:54:43 +0000

    Update and rename main.py to CoronaVirusNews.py

--- CoronaVirusNews.py
+++ CoronaVirusNews.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 # Program made by Yuki Matso ;)
-print("\n                   ***** Welcome to the Corona Virus News*****\n")
+print("\nWelcome to the Corona Virus News\n")
 source = requests.get("https://www.worldometers.info/coronavirus/").text
 soup = BeautifulSoup(source, 'lxml')
 print("".join(div.text for div in soup.find_all('div', id='maincounter-wrap')))
 while True :
-    country = input("                         *****To search about your country stats just type it name below, to quit please type ""exit""*****\n")
+    country = input("To search about your country stats just type it name below, to quit please type ""exit""\n")
     if country == "exit" :
         print("Thanks for using our program.")
         break
